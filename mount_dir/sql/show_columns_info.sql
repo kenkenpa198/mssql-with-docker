31d34ae074d3ee29aa3b2bc511dab47eef71d6ab
kenkenpa198 <kenkenpa198@gmail.com>
2022-02-09 22:04:01 +0900

    [update]デフォルト値も確認できるように更新。コメントいろいろ付記。

--- mount_dir/sql/show_columns_info.sql
+++ mount_dir/sql/show_columns_info.sql
@@ -5,36 +5,54 @@ SQL Server で指定したテーブルの下記の情報を一覧で取得する
 - データ型
 - 長さ
 - NULL 許可
+- デフォルト値
 
 ★のコメントを記述している箇所へ、情報を取得したいテーブル名を指定して実行してください。
 
 ▼参考サイト
-https://style.potepan.com/articles/24713.html
-https://ichiroku11.hatenablog.jp/entry/2015/12/20/213107
+- https://style.potepan.com/articles/24713.html            … ベースにさせていただきました。
+- https://ichiroku11.hatenablog.jp/entry/2015/12/20/213107 … PK 情報の結合を参考にしました。
+- https://johobase.com/sqlserver-catalogview-table-column/ … デフォルト値情報の結合と整形方法を参考にしました。
+
+加えて MS 公式のドキュメントも参考にしています。
+https://docs.microsoft.com/ja-jp/sql/relational-databases/system-catalog-views/object-catalog-views-transact-sql?view=sql-server-ver15
 
 *******************************************************************/
 
 SELECT
-    t.name                  AS 'テーブル名',
-    c.name                  AS 'カラム名',
+    o.name                    AS 'テーブル名',
+    c.name                    AS 'カラム名',
     CASE
-        WHEN is_primary_key = 1
+        WHEN pk.is_primary_key = 1
         THEN 'YES'
         ELSE 'NO'
-    END                     AS 'PK',
-    type_name(user_type_id) AS 'データ型',
-    max_length              AS '長さ',
+    END                       AS 'PK',
+    type_name(c.user_type_id) AS 'データ型',
+    c.max_length              AS '長さ',
     CASE
-        WHEN is_nullable = 1
+        WHEN c.is_nullable = 1
         THEN 'YES'
         ELSE 'NO'
-    END                     AS 'NULL 許可'
+    END                       AS 'NULL 許可',
+    CASE
+        -- デフォルト値に含まれている '((' と '))' を除去する
+        WHEN LEFT(d.definition, 2) = '((' AND RIGHT(d.definition, 2) = '))'
+        THEN SUBSTRING(d.definition, 3, LEN(d.definition) - 4)
+        -- デフォルト値に含まれている '(' と ')' を除去する
+        WHEN LEFT(d.definition, 1) = '(' AND RIGHT(d.definition, 1) = ')'
+        THEN SUBSTRING(d.definition, 2, LEN(d.definition) - 2)
+        ELSE NULL
+    END AS 'デフォルト値'
 
-FROM sys.objects t
-    INNER JOIN sys.columns c
-    ON t.object_id = c.object_id
+-- ベースとなる sys.objects カタログビューテーブル。このビューへ各情報を結合する。
+FROM sys.objects AS o
+
+    -- カラムのカタログビュー（カラム名やデータ型などの情報を保有）と内部結合
+    INNER JOIN sys.columns AS c
+    ON o.object_id = c.object_id
+
+    --インデックス関連のカタログビュー（PK の情報を保有）と外部結合
     LEFT OUTER JOIN (
-        -- PK を取得
         SELECT
             ic.object_id,
             ic.column_id,
@@ -46,15 +64,21 @@ FROM sys.objects t
                 AND i.index_id = ic.index_id
     ) pk
     ON
-        t.object_id = pk.object_id
+        o.object_id = pk.object_id
         AND c.column_id = pk.column_id
 
+    -- デフォルト制約のカタログビュー（デフォルト値の情報を保有）と外部結合
+    LEFT OUTER JOIN  sys.default_constraints AS d
+    ON
+        o.object_id = d.parent_object_id
+        AND c.column_id = d.parent_column_id
+
 WHERE
-    t.type = 'U'
-    AND t.name IN ('members') -- ★ここに確認したいテーブル名を指定。他に見たいテーブルがあれば IN の指定にカンマ区切りで追加可能。
+    o.type = 'U' -- オブジェクトの種類を「テーブル (ユーザー定義)」のみに制限
+    AND o.name IN ('members') -- ★ ここに確認したいテーブル名を指定してください。他に見たいテーブルがあれば IN の指定にカンマ区切りで追加可能です。
 
 -- テーブル名 > カラム ID の順に昇順で出力
 ORDER BY
-    t.name,
+    o.name,
     c.column_id
 ;
